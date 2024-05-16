86bae1f3b2ea23019ffac825061d8e5ebe899869
Xiaok <3795104+luxiaok@users.noreply.github.com>
2024-05-16 23:15:49 +0800

    make.sh: improve build feature

--- make.sh
+++ make.sh
@@ -2,11 +2,11 @@
 # Build for K-Wrt
 # Author: Xiaok
 # Date: 2024-03-18
-# Update: 2024-05-18
+# Update: 2024-05-16
 
 targets=$(cat targets.list | grep -Ev '^#')
 
-target_packages_default=build_config/package/common.config
+target_package_default=build_config/package/common.config
 
 proxy_server="192.168.188.91:1080"
 export http_proxy=socks5://${proxy_server}
@@ -23,16 +23,16 @@ function _log(){
 function build(){
     _target=$1
     _log "==================== ${_target} ===================="
-    target_meta=build_config/target/${_target}.config
-    target_packages=build_config/package/${_target}.config
-    [ -f ${target_packages} ] || target_packages=${target_packages_default}
+    target_profile=build_config/target/${_target}.config
+    target_package=build_config/package/${_target}.config
+    [ -f ${target_package} ] || target_package=${target_package_default}
     _log "${_target} => Generate build config file ..."
-    cat ${target_meta} ${target_packages} > .config
+    cat ${target_profile} ${target_package} > .config
     make defconfig
     backup_config_name="${_target}_`date +'%+4Y%m%d_%H%M'`".config
     _log "${_target} => Backup .config file to configs/${backup_config_name} ..."
     cp .config configs/${backup_config_name}
-    _log "${_target} => Run make world ..."
+    _log "${_target} => Make the world ..."
     make -j $jobs
     [ $? -eq 0 ] && _log "${_target} => Build completed."
 }
