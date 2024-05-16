2577f4b4e67fd86fed9164ddbe81b51fbea93a01
Xiaok <3795104+luxiaok@users.noreply.github.com>
2024-05-16 23:42:52 +0800

    make.sh: improve make feature

--- make.sh
+++ make.sh
@@ -4,15 +4,12 @@
 # Date: 2024-03-18
 # Update: 2024-05-16
 
-targets=$(cat targets.list | grep -Ev '^#')
-
+targets=targets.list
 target_package_default=build_config/package/common.config
-
 proxy_server="192.168.188.91:1080"
 export http_proxy=socks5://${proxy_server}
 export https_proxy=socks5://${proxy_server}
 export ftp_proxy=socks5://${proxy_server}
-
 jobs=$(nproc)
 
 function _log(){
@@ -21,29 +18,30 @@ function _log(){
 }
 
 function build(){
-    _target=$1
-    _log "==================== ${_target} ===================="
+    _target=$(echo $1 | awk -F: '{print $1}')
+    _target_name=$(echo $1 | awk -F: '{print $2}')
+    _log "==================== ${_target_name} ===================="
     target_profile=build_config/target/${_target}.config
     target_package=build_config/package/${_target}.config
     [ -f ${target_package} ] || target_package=${target_package_default}
-    _log "${_target} => Generate build config file ..."
+    _log "${_target_name} => Generate build config file ..."
     cat ${target_profile} ${target_package} > .config
     make defconfig
     backup_config_name="${_target}_`date +'%+4Y%m%d_%H%M'`".config
-    _log "${_target} => Backup .config file to configs/${backup_config_name} ..."
+    _log "${_target_name} => Backup .config file to configs/${backup_config_name} ..."
     cp .config configs/${backup_config_name}
-    _log "${_target} => Make the world ..."
+    _log "${_target_name} => Make the world ..."
     make -j $jobs
-    [ $? -eq 0 ] && _log "${_target} => Build completed."
+    [ $? -eq 0 ] && _log "${_target_name} => Build completed."
 }
 
 function run_build(){
     build_time=`date +"%F %T"`
     date +%s > version.date
     echo ${build_time} > feeds/kwrt/kwrt-settings/files/build.time
-    for i in $targets
+    grep -Ev '^#' ${targets_list} | while read line
     do
-        build $i
+        build "$line"
     done
 }
 
